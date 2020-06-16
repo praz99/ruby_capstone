6659ab5179f2f1249cb39e515a4275dc225b80d2
praz <t.prazwal@gmail.com>
2020-06-16 16:50:01 +0200

    add method to create the hash.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,21 +25,21 @@ class Scraper
     create_hash(table)
   end
 
-  # def create_hash(new_hash)
-  #   countries = []
-  #   new_hash.css('tbody').css('tr')[8..-1].each do |tr|
-  #     data = {
-  #       index: tr.css('td')[0].text,
-  #       name: tr.css('td')[1].text,
-  #       total: tr.css('td')[2].text,
-  #       active: tr.css('td')[7].text,
-  #       recovered: tr.css('td')[6].text,
-  #       deaths: tr.css('td')[4].text.strip
-  #     }
-  #     countries.push(data)
-  #   end
-  #   Pry.start(binding)
-  # end
+  def create_hash(new_hash)
+    countries = []
+    new_hash.css('tbody').css('tr')[8..-1].each do |tr|
+      data = {
+        index: tr.css('td')[0].text,
+        name: tr.css('td')[1].text,
+        total: tr.css('td')[2].text,
+        active: tr.css('td')[7].text,
+        recovered: tr.css('td')[6].text,
+        deaths: tr.css('td')[4].text.strip
+      }
+      countries.push(data)
+    end
+    Pry.start(binding)
+  end
 end
 
 scrap = Scraper.new('','','https://www.worldometers.info/coronavirus/#countries')
