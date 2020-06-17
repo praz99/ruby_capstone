94681e8f4e5f1834e69300760589a984a2d1f93f
praz <t.prazwal@gmail.com>
2020-06-17 17:02:04 +0200

    add test for Scraper class.

---
+++ spec/scraper_spec.rb
@@ -0,0 +1,12 @@
+require 'nokogiri'
+require 'httparty'
+require_relative '../lib/scraper.rb'
+
+describe Scraper do
+  let(:scraper) { Scraper.new }
+  context '#scrape' do
+    it 'returns an array of hash' do
+      expect(scraper.scrape[1]).to eql({:position=>"2", :country=>"Brazil", :total_cases=>"929,149", :active_cases=>"406,318", :recovered=>"477,364", :deaths=>"45,467"})
+    end
+  end
+end
