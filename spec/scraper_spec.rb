c72479064bf54ca1dfb73dd91b722ed3ac1b3126
praz <t.prazwal@gmail.com>
2020-06-17 17:16:28 +0200

    fix linter errors.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Metrics/LineLength
+
 require 'nokogiri'
 require 'httparty'
 require_relative '../lib/scraper.rb'
@@ -6,7 +8,8 @@ describe Scraper do
   let(:scraper) { Scraper.new }
   context '#scrape' do
     it 'returns an array of hash' do
-      expect(scraper.scrape[1]).to eql({:position=>"2", :country=>"Brazil", :total_cases=>"929,149", :active_cases=>"406,318", :recovered=>"477,364", :deaths=>"45,467"})
+      expect(scraper.scrape[1]).to eql({ position: '2', country: 'Brazil', total_cases: '929,149', active_cases: '406,318', recovered: '477,364', deaths: '45,467' })
     end
   end
 end
+# rubocop:enable Metrics/LineLength
