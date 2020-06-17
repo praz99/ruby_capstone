9aaa8779150b5c2c1a054b43c3f79ff6f9b1d4ce
praz <t.prazwal@gmail.com>
2020-06-17 09:52:36 +0200

    add methods to search by country name and position.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,46 +1,38 @@
-# frozen_string_literal: true
-
 require 'nokogiri'
 require 'httparty'
- require 'pry'
+require 'pry'
 # require 'open-uri'
 
 class Scraper
-  attr_reader :country, :position, :url
 
-  def initialize(country = nil, position = nil, url = nil)
-    @country = country
-    @position = position
-    @url = url
-  end
-
-  def parse_url(url)
-    page = HTTParty.get(url)
+  def parse_url()
+    page = HTTParty.get('https://www.worldometers.info/coronavirus/#countries')
     parse_page = Nokogiri::HTML(page)
   end
   
   def scrape
-    parsed_page = parse_url(@url)
+    parsed_page = parse_url()
     table = parsed_page.at('table')
     create_hash(table)
   end
 
+  private
   def create_hash(new_hash)
     countries = []
-    new_hash.css('tbody').css('tr')[8..-1].each do |tr|
+    new_hash.css('tbody').css('tr')[8..222].each do |tr|
       data = {
         index: tr.css('td')[0].text,
-        name: tr.css('td')[1].text,
+        name: tr.css('td')[1].text.capitalize,
         total: tr.css('td')[2].text,
-        active: tr.css('td')[7].text,
+        active: tr.css('td')[8].text,
         recovered: tr.css('td')[6].text,
         deaths: tr.css('td')[4].text.strip
       }
       countries.push(data)
     end
-    Pry.start(binding)
+    countries
   end
 end
 
-scrap = Scraper.new('','','https://www.worldometers.info/coronavirus/#countries')
-scrap.scrape
+# scrap = Scraper.new
+# scrap.scrape
--- lib/search.rb
+++ lib/search.rb
@@ -1,5 +1,29 @@
 require './scraper'
 
-class Search
+class Search < Scraper
+  def show_all()
+    scrape
+  end
 
+  def search_position(pos)
+    result = ''
+    
+    scrape.each do |item|
+      result = scrape[pos-1] if item[:index] == pos.to_s
+    end
+    result
+  end
+
+  def search_name(name)
+    result = ''
+    temp = ''
+    scrape.each do |item|
+      temp = item[:index] if item[:name] == name
+    end
+    result = scrape[temp.to_i-1]
+  end
 end
+
+# search = Search.new
+# search.search_name("Germany")
+# search.search_position(3)
