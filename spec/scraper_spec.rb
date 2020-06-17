7bb84cc92645224a363a6871f1782aef671ec713
praz <t.prazwal@gmail.com>
2020-06-17 17:43:13 +0200

    add test for #choice_valid and #position_valid.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -8,7 +8,7 @@ describe Scraper do
   let(:scraper) { Scraper.new }
   context '#scrape' do
     it 'returns an array of hash' do
-      expect(scraper.scrape[1]).to eql({ position: '2', country: 'Brazil', total_cases: '929,149', active_cases: '406,318', recovered: '477,364', deaths: '45,467' })
+      expect(scraper.scrape[212]).to eql({ position: '213', country: 'Anguilla', total_cases: '3', active_cases: '0', recovered: '3', deaths: '' })
     end
   end
 end
